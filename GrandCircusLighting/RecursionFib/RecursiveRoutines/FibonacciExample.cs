1c17da145377fcd021a47a856d6657c49137c06b
sldeskins <sldeskins@ymail.com>
2016-04-12 19:59:54 -0400

    Recursion examples

---
+++ GrandCircusLighting/RecursionFib/RecursiveRoutines/FibonacciExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursiveRoutines
+{
+    public class FibonacciExample
+    {
+        public static int Fib(int n) {
+            if (n<0) {
+                throw new ArgumentOutOfRangeException("The value must be an integer greater than or equal to zero."); 
+            }else if (n == 0) {
+                return 0;
+            }
+            else if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fib(n-1)+Fib(n-2);
+            }
+        }
+    }
+}
---
+++ GrandCircusLighting/RecursionFib/RecursiveRoutinesTest/FibonacciExampleTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RecursiveRoutines;
+
+namespace RecursiveRoutinesTest
+{
+    [TestClass]
+    public class FibonacciExampleTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "The value must be an integer greater than or equal to zero.")]
+        public void InvalidRange()
+        {
+            int result = FibonacciExample.Fib(-987);
+        }
+        [TestMethod]
+        public void N0()
+        {
+            int result = FibonacciExample.Fib(0);
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void N1()
+        {
+            int result = FibonacciExample.Fib(1);
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void N6()
+        {
+            int result = FibonacciExample.Fib(6);
+            Assert.AreEqual(8, result);
+        }
+    }
+}
